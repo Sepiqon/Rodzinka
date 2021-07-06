6d4255ca0da2ea97dc6701dd9f25df08366ec8a2
start.vaadin.com <no-reply@vaadin.com>
2021-07-06 18:56:13 +0000

    Generated project

---
+++ src/main/java/pl/sepiqon/rodzinka/views/about/AboutView.java
@@ -0,0 +1,18 @@
+package pl.sepiqon.rodzinka.views.about;
+
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.PageTitle;
+import pl.sepiqon.rodzinka.views.MainLayout;
+
+@Route(value = "about", layout = MainLayout.class)
+@PageTitle("About")
+public class AboutView extends Div {
+
+    public AboutView() {
+        addClassName("about-view");
+        add(new Text("Content placeholder"));
+    }
+
+}
